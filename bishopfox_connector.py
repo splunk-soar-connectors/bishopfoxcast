09773339f384af5f78f53e3cd5f46c248e3ba73b
Daniel Federschmidt <daniel@federschmidt.xyz>
2023-02-03 13:56:02 +0100

    Bishop Fox Cast: Bugfix - Capitalization in _build_container_json (#1)
    * Update bishopfox_connector.py
    
    * Update bishopfox_connector.py
    
    * always capitalize
    
    * Dev checklist changes
    
    * Code review changes
    
    * Code review change
    
    ---------
    
    Co-authored-by: Dhvani Vora <dvora@splunk.com>

--- bishopfox_connector.py
+++ bishopfox_connector.py
@@ -1,6 +1,6 @@
 # File: bishopfox_connector.py
 #
-# Copyright (c) 2021 Splunk Inc.
+# Copyright (c) 2021-2023 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,23 +17,22 @@
 # Python 3 Compatibility imports
 from __future__ import print_function, unicode_literals
 
+import json
+from datetime import datetime
+from urllib.parse import unquote
+
+import dateutil.parser
 # Phantom App imports
 import phantom.app as phantom
-from phantom.base_connector import BaseConnector
+import requests
+from bs4 import BeautifulSoup
 from phantom.action_result import ActionResult
+from phantom.base_connector import BaseConnector
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 # Usage of the consts file is recommended
 import bishopfox_consts as consts
-import requests
-import json
-from bs4 import BeautifulSoup
-
-import dateutil.parser
-from datetime import datetime
-
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
-from urllib.parse import unquote
 
 
 class RetVal(tuple):
@@ -157,7 +156,7 @@ class BishopFoxConnector(BaseConnector):
                 elif len(e.args) == 1:
                     error_code = consts.ERR_CODE_MSG
                     error_msg = e.args[0]
-        except:
+        except Exception:
             pass
 
         return "Error Code: {0}. Error Message: {1}".format(error_code, error_msg)
@@ -256,7 +255,9 @@ class BishopFoxConnector(BaseConnector):
 
     def _build_container_json(self, finding):
         label = self.get_config().get("ingest", {}).get("container_label")
-        severity = consts.SEVERITY_MAP[finding["severity"]]
+        finding_severity = finding["severity"].capitalize()
+
+        severity = consts.SEVERITY_MAP[finding_severity]
         container_json = {
             "name": finding.get("category"),
             "label": label,
@@ -286,7 +287,7 @@ class BishopFoxConnector(BaseConnector):
             # the 'since' parameter does not include time information, only the date
             try:
                 params["since"] = dateutil.parser.isoparse(kwargs["since"]).strftime("%Y-%m-%d")
-            except:
+            except Exception:
                 error_message = "Please provide a valid 'since' action parameter"
                 return RetVal(action_result.set_status(phantom.APP_ERROR, error_message), None)
         if kwargs.get("status"):
@@ -355,6 +356,8 @@ class BishopFoxConnector(BaseConnector):
             # action failed, this should already be captured in the action result
             return action_result.get_status()
 
+        self.save_progress(f"Total findings: {len(findings)}")
+
         # Add the response into the data section
         action_result.add_data(findings)
 
@@ -396,6 +399,8 @@ class BishopFoxConnector(BaseConnector):
             # the call to the 3rd party device or service failed, action result should contain all the error details
             return action_result.get_status()
 
+        self.save_progress("Successfully Updated Status")
+
         # Add the response into the data section
         data = [self._parse_subject_json(subj, finding_uid) for subj in response]
         action_result.add_data(data)
@@ -430,6 +435,8 @@ class BishopFoxConnector(BaseConnector):
             # the call to the 3rd party device or service failed, action result should contain all the error details
             return action_result.get_status()
 
+        self.save_progress("Successfully Updated Client ID")
+
         # Add the response into the data section
         data = [self._parse_subject_json(subj, finding_uid) for subj in response]
         action_result.add_data(data)
@@ -463,6 +470,8 @@ class BishopFoxConnector(BaseConnector):
         if phantom.is_fail(ret_val):
             # the call to the 3rd party device or service failed, action result should contain all the error details
             return action_result.get_status()
+
+        self.save_progress("Successfully Updated Client Note")
 
         # Add the response into the data section
         data = [self._parse_subject_json(subj, finding_uid) for subj in response]
@@ -591,8 +600,10 @@ class BishopFoxConnector(BaseConnector):
 
 
 def main():
-    import pudb
     import argparse
+    import sys
+
+    import pudb
 
     pudb.set_trace()
 
@@ -636,7 +647,7 @@ def main():
             session_id = r2.cookies["sessionid"]
         except Exception as e:
             print("Unable to get session id from the platform. Error: " + str(e))
-            exit(1)
+            sys.exit(1)
 
     with open(args.input_test_json) as f:
         in_json = f.read()
@@ -653,7 +664,7 @@ def main():
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    exit(0)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
